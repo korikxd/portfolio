556dda9f2728c220f17609bd8f8d14c6e351b8a4
Gabriel <gabriel.garmer01@gmail.com>
2023-04-28 20:20:00 -0400

    🤡 CustomLayoutContent component

---
+++ src/core/CustomLayout/components/CustomLayoutContent/index.jsx
@@ -0,0 +1,15 @@
+import CharacterCard from '@components/CharacterCard'
+import ProjectCarousel from '@components/ProjectCarousel'
+import SkillCard from '@components/SkillCard'
+
+const CustomLayoutContent = () => {
+  return (
+    <>
+      <CharacterCard/>
+      <SkillCard/>
+      <ProjectCarousel/>
+    </>
+  )
+}
+
+export default CustomLayoutContent
