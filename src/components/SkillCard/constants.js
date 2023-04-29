e6ab4645e6b525b0fd614e9697cc87b1728b2770
Gabriel <gabriel.garmer01@gmail.com>
2023-04-28 20:21:19 -0400

    🚧 Temp save skills array inside a constant file

---
+++ src/components/SkillCard/constants.js
@@ -0,0 +1,32 @@
+const FRONTEND_SKILLS = [
+   { name: '', icon: '', points: 4 },
+   { name: '', icon: '', points: 3 },
+   { name: '', icon: '', points: 3 },
+   { name: '', icon: '', points: 3 },
+   { name: '', icon: '', points: 3 },
+   { name: '', icon: '', points: 2 },
+]
+
+const BACKEND_SKILLS = [
+   { name: '', icon: '', points: 3 },
+   { name: '', icon: '', points: 3 },
+   { name: '', icon: '', points: 2 },
+   { name: '', icon: '', points: 2 },
+   { name: '', icon: '', points: 2 },
+   { name: '', icon: '', points: 2 },
+]
+
+const TOOLS_SKILLS = [
+   { name: '', icon: '', points: 4 },
+   { name: '', icon: '', points: 4 },
+   { name: '', icon: '', points: 4 },
+   { name: '', icon: '', points: 4 },
+   { name: '', icon: '', points: 4 },
+   { name: '', icon: '', points: 3 },
+]
+
+export const SKILL_CARDS = [
+   { key: 'front', title: 'Frontend', skills: FRONTEND_SKILLS },
+   { key: 'back', title: 'Backend', skills: BACKEND_SKILLS},
+   { key: 'tools', title: 'Tools', skills: TOOLS_SKILLS },
+]
