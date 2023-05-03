c8f9c0533443c13b3d4f1c8e4f4c812f2f9b60aa
Gabriel <gabriel.garmer01@gmail.com>
2023-05-03 10:15:20 -0400

    Revert "✨"
    This reverts commit 909204cf923115ef38b54671805d7ba059eba018.

--- src/components/SocialMediaCard/constants.js
+++
@@ -1,6 +0,0 @@
-export const SOCIAL_MEDIA = [
-   { name: 'linkedin', link: 'https://www.linkedin.com/in/gabriel-garc%C3%ADa-mercado/' },
-   { name: 'github', link: 'https://github.com/gabgarmer' },
-   { name: 'youtube', link: 'https://www.youtube.com/@korikxd' },
-   { name: 'resume', link: '' },
-]
