5adfe9d4137c6cd38238a914cd07ee0044889e04
Gabriel <gabriel.garmer01@gmail.com>
2023-05-03 00:20:18 -0400

    ♻️ Refactor app file & use of context

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { PortfolioProvider } from '@lib/Context/PortfolioContext'
+import PropTypes from 'prop-types'
 
-export default function App({ Component, pageProps }) {
-   return (
-      <Component {...pageProps} />
-   )
+require('./../styles/base.less')
+
+const MyApp = ({ Component, pageProps }) => (
+  <PortfolioProvider>
+    <Component {...pageProps}/>
+  </PortfolioProvider>
+)
+
+export default MyApp
+
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any
 }
