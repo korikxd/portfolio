3cc683bb41c2d9ff0619676a6131892d0e88fc28
Gabriel <gabriel.garmer01@gmail.com>
2023-05-03 10:13:25 -0400

    🤡 Mock CustomLayoutHeader component

--- src/core/CustomLayout/components/CustomLayoutHeader/index.jsx
+++ src/core/CustomLayout/components/CustomLayoutHeader/index.jsx
@@ -1,9 +1,16 @@
+import { Col, Row } from 'antd'
+import SelectLanguage from '@components/SelectLanguage'
+import ThemeButton from '@components/ThemeButton'
 
-
-const CustomLayoutHeader = () => {
-  return (
-    <></>
-  )
-}
+const CustomLayoutHeader = () => (
+  <Row gutter={8} justify='end'>
+    <Col>
+      <SelectLanguage/>
+    </Col>
+    <Col>
+      <ThemeButton/>
+    </Col>
+  </Row>
+)
 
 export default CustomLayoutHeader
