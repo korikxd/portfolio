76361d2456aa2d642809b4045d45a6907b68c4ae
Gabriel <gabriel.garmer01@gmail.com>
2023-05-03 10:12:48 -0400

    ♻️ Render SectionTitle component

--- src/core/CustomLayout/components/CustomLayoutContent/index.jsx
+++ src/core/CustomLayout/components/CustomLayoutContent/index.jsx
@@ -1,15 +1,16 @@
 import CharacterCard from '@components/CharacterCard'
 import ProjectCarousel from '@components/ProjectCarousel'
+import SectionTitle from '@components/SectionTitle'
 import SkillCard from '@components/SkillCard'
 
-const CustomLayoutContent = () => {
-  return (
-    <>
-      <CharacterCard/>
-      <SkillCard/>
-      <ProjectCarousel/>
-    </>
-  )
-}
+const CustomLayoutContent = () => (
+  <>
+    <CharacterCard/>
+    <SectionTitle title={'SKILLS'}/>
+    <SkillCard/>
+    <SectionTitle title={'PROJECTS'}/>
+    <ProjectCarousel/>
+  </>
+)
 
 export default CustomLayoutContent
