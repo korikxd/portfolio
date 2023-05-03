a9bacbfa7dfc04923b74b17fc613f890e9746c41
Gabriel <gabriel.garmer01@gmail.com>
2023-05-03 10:12:22 -0400

    ♻️  Move tabulation on CustomLayout component

--- src/core/CustomLayout/index.jsx
+++ src/core/CustomLayout/index.jsx
@@ -7,17 +7,17 @@ import CustomLayoutHeader from './components/CustomLayoutHeader'
 const { Header, Content, Footer } = Layout
 
 const CustomLayout = () => (
-    <Layout>
-      <Header>
-        <CustomLayoutHeader/>
-      </Header>
-      <Content>
-        <CustomLayoutContent/>
-      </Content>
-      <Footer>
-        <CustomLayoutFooter/>
-      </Footer>
-    </Layout>
-  )
+  <Layout>
+    <Header>
+      <CustomLayoutHeader/>
+    </Header>
+    <Content>
+      <CustomLayoutContent/>
+    </Content>
+    <Footer>
+      <CustomLayoutFooter/>
+    </Footer>
+  </Layout>
+)
 
 export default CustomLayout
