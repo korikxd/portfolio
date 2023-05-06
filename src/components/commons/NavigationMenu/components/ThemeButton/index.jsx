e373130f46de09309af7e748fd4d5ec694228f12
Gabriel <gabriel.garmer01@gmail.com>
2023-05-06 19:08:35 -0400

    🤡 ThemeButton component

---
+++ src/components/commons/NavigationMenu/components/ThemeButton/index.jsx
@@ -0,0 +1,28 @@
+import { IoMdMoon as MoonIcon } from 'react-icons/io'
+import { IoSunnyOutline as SunIcon } from 'react-icons/io5'
+import usePortfolioContext from '@lib/Context/PortfolioContext'
+
+import styles from './styles.module.less'
+
+const ThemeButton = () => {
+
+  const { currentTheme, setCurrentTheme } = usePortfolioContext()
+  const changeTheme = (theme) => theme === 'light' ? setCurrentTheme('dark') : setCurrentTheme('light')
+
+  return (
+    <button
+      type='button'
+      className={styles.theme_button}
+      onClick={() => changeTheme(currentTheme)}
+    >
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        viewBox='0 0 20 20'
+      >
+        {currentTheme === 'light' ? <SunIcon/> : <MoonIcon/>}
+      </svg>
+    </button>
+  )
+}
+
+export default ThemeButton
