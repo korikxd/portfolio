88b42e63adda3d08f4718dc137c704f899617dae
Gabriel <gabriel.garmer01@gmail.com>
2023-05-06 19:08:56 -0400

    🤡 Banner component

---
+++ src/components/modules/HomePage/components/Banner/index.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.less'
+
+const Banner = () => (
+  <>
+    <div className={styles.banner_container}>
+      <span className={styles.banner_paragraph}>Gabriel García</span>
+      <p className={styles.banner_paragraph}>Software Engineer</p>
+    </div>
+  </>
+)
+
+export default Banner
