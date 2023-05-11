546e0af7f2d6003fd273560645b4efba86a1cd60
Gabriel <gabriel.garmer01@gmail.com>
2023-05-11 19:18:03 -0400

    🤡 AboutPage component

--- src/components/modules/AboutPage/index.jsx
+++ src/components/modules/AboutPage/index.jsx
@@ -1,13 +1,33 @@
+import { Col, Row } from 'antd'
 import CustomLayout from '@core/CustomLayout'
 
-const AboutPage = () => {
+import BiographyCard from './components/BiographyCard'
+import ExperienceCard from './components/ExperienceCard'
+import ProfilePictureCard from './components/ProfilePictureCard'
+import SkillsCard from './components/SkillsCard'
 
+const AboutPage = () => (
+  <CustomLayout>
+    <Row gutter={16} justify='center'>
+      <Col span={4}>
+        <ProfilePictureCard/>
+      </Col>
+      <Col>
+        <BiographyCard/>
+      </Col>
+    </Row>
+    <Row justify='center'>
+      <Col span={10}>
+        <SkillsCard/>
+      </Col>
+    </Row>
+    <Row justify='center'>
+      <Col span={10}>
+        <ExperienceCard/>
+      </Col>
+    </Row>
+  </CustomLayout>
+)
 
-  return (
-    <CustomLayout>
-      <></>
-    </CustomLayout>
-  )
-}
 
 export default AboutPage
