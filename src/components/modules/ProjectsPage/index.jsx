b870e9168fba949599c1013685cb92edfe126ecc
Gabriel <gabriel.garmer01@gmail.com>
2023-05-17 18:14:05 -0400

    🤡 ProjectPage component

--- src/components/modules/ProjectsPage/index.jsx
+++ src/components/modules/ProjectsPage/index.jsx
@@ -1,10 +1,45 @@
+import { Col, Row } from 'antd'
 import CustomLayout from '@core/CustomLayout'
+import usePortfolioContext from '@lib/Context/PortfolioContext'
+
+import { ALL_PROJECTS, HORIZONTAL_GUTTER, VERTICAL_GUTTER } from './constants'
+import ProjectCard from './components/ProjectCard'
+import SectionTitle from './components/SectionTitle'
 
 const ProjectsPage = () => {
 
+  const { currentLanguage } = usePortfolioContext()
+
+  const renderAllProjects = (projects) => (
+    <>
+      <SectionTitle sectionTitle={currentLanguage === 'ESPAÑOL' ? 'COLECCIÓN' : 'HISTORIC'}/>
+      <Row gutter={[HORIZONTAL_GUTTER , VERTICAL_GUTTER]}>
+      {projects.map((project, index) => (
+        <Col key={index} span={6}>
+          <ProjectCard
+            projectImage={project.image}
+            projectName={project.name}
+            projectDescription={project.description}
+            projectLink={project.link}
+          />
+        </Col>
+      ))}
+      </Row>
+    </>
+  )
+
   return (
     <CustomLayout>
-      <></>
+      <Row justify='center'>
+        <Col>
+          <h1>{currentLanguage === 'ESPAÑOL' ? 'PROYECTOS' : 'PROJECTS'}</h1>
+        </Col>
+      </Row>
+      <Row justify='center'>
+        <Col span={24}>
+          {renderAllProjects(ALL_PROJECTS)}
+        </Col>
+      </Row>
     </CustomLayout>
   )
 }
