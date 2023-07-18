d21a657de7e5540b4c357eca75bc1fecc7c78b5a
korikxd <gabriel.garmer01@gmail.com>
2023-07-18 16:46:13 -0400

    🔧 Modified config file

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
-const withAntdLess = require('next-plugin-antd-less')
-
-const nextConfig = withAntdLess ({
-   lessLoaderOptions: {},
-   lessVarsFilePath: './src/styles/antd.less',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
    eslint: {
       ignoreDuringBuilds: true,
       dirs: ['pages', 'components', 'lib']
    },
-   env: {},
-   webpack(config) {
-      config.module.rules.push({
-         test: /\.svg$/i,
-         issuer: /\.[jt]sx?$/,
-         use: ['@svgr/webpack']
-      })
-      return config
-   }
-})
+   env: {}
+}
 
 module.exports = nextConfig
