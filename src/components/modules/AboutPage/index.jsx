9f09ec3e74a76d8a20c1d2f88f1014b28af62ca9
korikxd <gabriel.garmer01@gmail.com>
2023-07-26 19:20:25 -0400

    🚧 Removed antd grids

--- src/components/modules/AboutPage/index.jsx
+++ src/components/modules/AboutPage/index.jsx
@@ -1,32 +1,23 @@
-import { Col, Row } from 'antd'
-
 import BiographyCard from './components/BiographyCard'
 import ExperienceCard from './components/ExperienceCard'
 import ProfilePictureCard from './components/ProfilePictureCard'
 import SkillsCard from './components/SkillsCard'
 
 const AboutPage = () => (
-  <>
-    <Row gutter={16} justify='center'>
-      <Col span={4}>
-        <ProfilePictureCard/>
-      </Col>
-      <Col>
-        <BiographyCard/>
-      </Col>
-    </Row>
-    <Row justify='center'>
-      <Col span={10}>
-        <SkillsCard/>
-      </Col>
-    </Row>
-    <Row justify='center'>
-      <Col span={10}>
-        <ExperienceCard/>
-      </Col>
-    </Row>
-  </>
+  <div className="flex flex-col justify-center">
+    <div className="">
+      <ProfilePictureCard />
+    </div>
+    <div className="">
+      <BiographyCard />
+    </div>
+    <div className="">
+      <SkillsCard />
+    </div>
+    <div className="">
+      <ExperienceCard />
+    </div>
+  </div>
 )
-
 
 export default AboutPage
