bb11f7b0549fab19921b1b46cb6bb9be7acbd5ee
korikxd <gabriel.garmer01@gmail.com>
2023-07-26 19:22:54 -0400

    🚧 Removed antd components

--- src/components/modules/HomePage/components/SocialButtons/index.jsx
+++ src/components/modules/HomePage/components/SocialButtons/index.jsx
@@ -1,27 +1,20 @@
-import { Button, Col, Row } from 'antd'
 import Link from 'next/link'
 import { SOCIAL_LINKS } from './constants'
 
 const SocialButtons = () => {
-
   const renderButtons = () => (
-    <Row gutter={8} justify='center'>
-      {
-        SOCIAL_LINKS.map((social, index) => (
-          <Col key={index}>
-            <Button shape='circle'>
-              <Link target='_blank' rel='noopener noreferrer' href={social.link}>{social.icon}</Link>
-            </Button>
-          </Col>
-        ))
-      }
-    </Row>
+    <div className="">
+      {SOCIAL_LINKS.map((social, index) => (
+        <button className="">
+          <Link target="_blank" rel="noopener noreferrer" href={social.link}>
+            {social.icon}
+          </Link>
+        </button>
+      ))}
+    </div>
   )
 
-  return (
-    renderButtons()
-  )
+  return renderButtons()
 }
-
 
 export default SocialButtons
