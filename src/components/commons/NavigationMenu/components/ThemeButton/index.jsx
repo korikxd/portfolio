59d4aba5d5ee2a630d0ce765a3124a6289f875f0
korikxd <gabriel.garmer01@gmail.com>
2023-07-26 19:20:06 -0400

    💄 Modified toggle app mode component

--- src/components/commons/NavigationMenu/components/ThemeButton/index.jsx
+++ src/components/commons/NavigationMenu/components/ThemeButton/index.jsx
@@ -1,22 +1,33 @@
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
 import { IoMdMoon as MoonIcon } from 'react-icons/io'
 import { IoSunnyOutline as SunIcon } from 'react-icons/io5'
-import usePortfolioContext from '@lib/Context/PortfolioContext'
 
 const ThemeButton = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme, resolvedTheme } = useTheme()
+  const changeTheme = (theme) => (theme === 'dark' || resolvedTheme === 'dark' ? setTheme('light') : setTheme('dark'))
 
-  const { currentTheme, setCurrentTheme } = usePortfolioContext()
-  const changeTheme = (theme) => theme === 'light' ? setCurrentTheme('dark') : setCurrentTheme('light')
+  useEffect(() => setMounted(true), [])
 
   return (
     <button
-      type='button'
-      onClick={() => changeTheme(currentTheme)}
+      aria-label="Toggle Dark Mode"
+      type="button"
+      className="ml-1 mr-1 h-8 w-8 rounded p-1 sm:ml-4"
+      onClick={changeTheme}
     >
       <svg
-        xmlns='http://www.w3.org/2000/svg'
-        viewBox='0 0 20 20'
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="text-gray-900 dark:text-gray-100"
       >
-        {currentTheme === 'light' ? <SunIcon/> : <MoonIcon/>}
+        {mounted && (theme === 'dark' || resolvedTheme === 'dark') ? (
+          <SunIcon className="fade-in text-xl" />
+        ) : (
+          <MoonIcon className="fade-in text-xl" />
+        )}
       </svg>
     </button>
   )
