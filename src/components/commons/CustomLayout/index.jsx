5f57e3c57ab71c3b1c446e6b69d1ba43a3c17d90
korikxd <gabriel.garmer01@gmail.com>
2023-08-05 17:21:32 -0400

    🤡 Responsive layout

--- src/components/commons/CustomLayout/index.jsx
+++ src/components/commons/CustomLayout/index.jsx
@@ -3,16 +3,14 @@ import NavigationMenu from '../NavigationMenu'
 
 const CustomLayout = ({ children }) => {
   return (
-    <div>
-      <div className="flex flex-col h-screen justify-between">
-        <header className="flex items-center justify-end py-8">
-          <NavigationMenu />
-        </header>
-        <main className="mb-auto">{children}</main>
-        <div className="flex flex-col mt-4 items-center py-8 font-light dark:text-white lg:py-8">
-          <Footer />
-        </div>
-      </div>
+    <div className="flex flex-col h-screen justify-between">
+      <header className="flex items-center justify-end py-8">
+        <NavigationMenu />
+      </header>
+      <main className="flex flex-col grow">{children}</main>
+      <footer className="flex flex-col items-center py-8 font-light dark:text-white lg:py-8">
+        <Footer />
+      </footer>
     </div>
   )
 }
