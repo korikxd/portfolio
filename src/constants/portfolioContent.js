bc0c7de718d3b6d87d31c27dac848a795a3f11ff
korikxd <gabriel.garmer01@gmail.com>
2023-08-05 17:20:38 -0400

    🚧 Default context object

---
+++ src/constants/portfolioContent.js
@@ -0,0 +1,17 @@
+export const PORTFOLIO_DEFAULT_VALUES = {
+  portfolio: {
+    language: 'ESPAÑOL',
+    spanish_content: {
+      homePageText: {},
+      aboutPageText: {},
+      projectsPageText: {},
+      contactPageText: {}
+    },
+    english_content: {
+      homePageText: {},
+      aboutPageText: {},
+      projectsPageText: {},
+      contactPageText: {}
+    }
+  }
+}
