843f0ef39f131d136f0d83d48ea7543139404e35
korikxd <gabriel.garmer01@gmail.com>
2023-08-13 11:07:07 -0400

    ♻️ Use of react icons instead antd

--- src/components/modules/HomePage/components/SocialButtons/constants.js
+++ src/components/modules/HomePage/components/SocialButtons/constants.js
@@ -1,7 +1,7 @@
-import { GithubFilled, LinkedinFilled, YoutubeFilled } from '@ant-design/icons'
+import { SiYoutube, SiGithub, SiLinkedin } from 'react-icons/si'
 
 export const SOCIAL_LINKS = [
-  { name: 'Github', link: 'https://github.com/gabgarmer', icon: <GithubFilled /> },
-  { name: 'Youtube', link: 'https://www.youtube.com/@korikxd', icon: <YoutubeFilled /> },
-  { name: 'Linkedin', link: 'https://www.linkedin.com/in/gabriel-garc%C3%ADa-mercado/', icon: <LinkedinFilled /> }
+  { name: 'Github', link: 'https://github.com/gabgarmer', icon: <SiYoutube /> },
+  { name: 'Youtube', link: 'https://www.youtube.com/@korikxd', icon: <SiGithub /> },
+  { name: 'Linkedin', link: 'https://www.linkedin.com/in/gabriel-garc%C3%ADa-mercado/', icon: <SiLinkedin /> }
 ]
