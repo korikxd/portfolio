b0b29974d553e982607df94592df8a2882d9813e
korikxd <gabriel.garmer01@gmail.com>
2023-08-13 11:25:22 -0400

    ♻️ Use of template literals

--- src/components/modules/HomePage/components/Banner/index.jsx
+++ src/components/modules/HomePage/components/Banner/index.jsx
@@ -1,6 +1,6 @@
 const Banner = () => (
-  <div className="h-1/3 flex flex-col justify-center bg-gradient-to-r from-[#eb3a5b] to-[#fe5000]">
-    <p className="text-center font-bold">
+  <div className={`bg-gradient-to-r from-[#eb3a5b] to-[#fe5000]`}>
+    <p className={`text-center font-bold`}>
       Gabriel García
       <br />
       Software Engineer
