28eb6d519d9645e457097bf2bd125f5399d53b81
korikxd <gabriel.garmer01@gmail.com>
2023-08-14 22:05:42 -0400

    ✨ Constant values to use inside Projects page

--- src/components/modules/ProjectsPage/constants.js
+++ src/components/modules/ProjectsPage/constants.js
@@ -1,32 +1,44 @@
+import { SPANISH_RIBBONS } from './components/StatusRibbon/constants'
+
 export const ALL_PROJECTS = [
   {
-    image: '/profile_picture.png',
+    image: 'profile_picture.png',
     name: 'Primer proyecto',
     description: 'Lorem ipsum',
-    link: ''
+    link: '',
+    tags: [],
+    status: SPANISH_RIBBONS[0]
   },
   {
-    image: '/profile_picture.png',
+    image: 'profile_picture.png',
     name: 'Segundo proyecto',
     description: 'Lorem ipsum',
-    link: ''
+    link: '',
+    tags: [],
+    status: SPANISH_RIBBONS[1]
   },
   {
-    image: '/profile_picture.png',
+    image: 'profile_picture.png',
     name: 'Tercer proyecto',
     description: 'Lorem ipsum',
-    link: ''
+    link: '',
+    tags: [],
+    status: SPANISH_RIBBONS[2]
   },
   {
-    image: '/profile_picture.png',
+    image: 'profile_picture.png',
     name: 'Cuarto proyecto',
     description: 'Lorem ipsum',
-    link: ''
+    link: '',
+    tags: [],
+    status: SPANISH_RIBBONS[3]
   },
   {
-    image: '/profile_picture.png',
+    image: 'profile_picture.png',
     name: 'Quinto proyecto',
     description: 'Lorem ipsum',
-    link: ''
+    link: '',
+    tags: [],
+    status: SPANISH_RIBBONS[0]
   }
 ]
