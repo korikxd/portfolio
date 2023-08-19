69482e23a72d9d83239d1dd7ef0a8273df96b165
korikxd <gabriel.garmer01@gmail.com>
2023-08-18 20:52:29 -0400

    ♻️ Pass context variables as params

--- src/components/modules/HomePage/index.jsx
+++ src/components/modules/HomePage/index.jsx
@@ -1,13 +1,23 @@
+import { useEffect } from 'react'
 import Banner from './components/Banner'
 import NavigationButtons from './components/NavigationButtons'
 import SocialButtons from './components/SocialButtons'
+import { ENGLISH_TEXTS, SPANISH_TEXTS } from './components/NavigationButtons/constants'
 
-const HomePage = () => {
+const HomePage = ({ homeData, setHomeData, language }) => {
+  useEffect(() => {
+    if (language !== 'ESPAÑOL') {
+      setHomeData({ ...homeData, navigationButtons: ENGLISH_TEXTS })
+    } else {
+      setHomeData({ ...homeData, navigationButtons: SPANISH_TEXTS })
+    }
+  }, [language])
+
   return (
     <div className={`h-full`}>
       <Banner />
-      <NavigationButtons />
-      <SocialButtons />
+      <NavigationButtons buttons={homeData.navigationButtons} />
+      <SocialButtons socials={homeData.socials} />
     </div>
   )
 }
