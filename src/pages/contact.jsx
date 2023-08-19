da5ab0544bfae240febe8f3dfd9655de1b24b89b
korikxd <gabriel.garmer01@gmail.com>
2023-08-18 20:42:38 -0400

    ♻️ Import context on each page file

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,24 @@
 import ContactPage from '@components/modules/ContactPage'
+import usePortfolioContext from '@lib/Context/PortfolioContext'
 
-const Contact = () => <ContactPage/>
+const Contact = () => {
+  const {
+    portfolioContent: {
+      content: { contactPageText },
+      language
+    },
+    setContactContent
+  } = usePortfolioContext()
+
+  return <ContactPage contactData={contactPageText} setContactData={setContactContent} language={language} />
+}
 
 export default Contact
+
+export const getServerSideProps = async (context) => {
+  return {
+    props: {
+      data: {}
+    }
+  }
+}
