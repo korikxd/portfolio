637c6e0f7fe561f62e23d2c11f53bc439e0c135f
korikxd <gabriel.garmer01@gmail.com>
2023-08-18 20:45:34 -0400

    ♻️ Sets language related to context

--- src/components/modules/AboutPage/index.jsx
+++ src/components/modules/AboutPage/index.jsx
@@ -1,15 +1,41 @@
+import { useEffect } from 'react'
 import BiographyCard from './components/BiographyCard'
 import ExperienceCard from './components/ExperienceCard'
 import ProfilePictureCard from './components/ProfilePictureCard'
 import SkillsCard from './components/SkillsCard'
+import { ENGLISH_BIOGRAPHY_PARAGRAPHS, SPANISH_BIOGRAPHY_PARAGRAPHS } from './components/BiographyCard/constants'
+import { LANGUAGES_ENGLISH, LANGUAGES_SPANISH, SKILLS_ENGLISH, SKILLS_SPANISH } from './components/SkillsCard/constants'
+import { EXPERIENCES_ENGLISH, EXPERIENCES_SPANISH } from './components/ExperienceCard/constants'
 
-const AboutPage = () => (
-  <div className={`flex flex-col items-center gap-16`}>
-    <ProfilePictureCard />
-    <BiographyCard />
-    <SkillsCard />
-    <ExperienceCard />
-  </div>
-)
+const AboutPage = ({ aboutData, setAboutData, language }) => {
+  useEffect(() => {
+    if (language !== 'ESPAÑOL') {
+      setAboutData({
+        ...aboutData,
+        biographyTexts: ENGLISH_BIOGRAPHY_PARAGRAPHS,
+        skills: SKILLS_ENGLISH,
+        languages: LANGUAGES_ENGLISH,
+        experiences: EXPERIENCES_ENGLISH
+      })
+    } else {
+      setAboutData({
+        ...aboutData,
+        biographyTexts: SPANISH_BIOGRAPHY_PARAGRAPHS,
+        skills: SKILLS_SPANISH,
+        languages: LANGUAGES_SPANISH,
+        experiences: EXPERIENCES_SPANISH
+      })
+    }
+  }, [language])
+
+  return (
+    <div className={`flex flex-col items-center gap-16`}>
+      <ProfilePictureCard profilePictureRoute={aboutData.profilePicture} />
+      <BiographyCard biographyTexts={aboutData.biographyTexts} currentLanguage={language} />
+      <SkillsCard skills={aboutData.skills} languages={aboutData.languages} />
+      <ExperienceCard experiences={aboutData.experiences} />
+    </div>
+  )
+}
 
 export default AboutPage
