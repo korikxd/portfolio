a7c9ea01ebf8784182733504c4e3414924544baa
korikxd <gabriel.garmer01@gmail.com>
2023-08-18 20:49:16 -0400

    ♻️ Separate spanish & english constants

---
+++ src/components/modules/ContactPage/constants.js
@@ -0,0 +1,9 @@
+export const CONTACT_SPANISH_TEXTS = {
+  title: 'HABLEMOS',
+  paragraph: `Siéntete libre de mandarme un mensaje, trataré de responder a la brevedad.`
+}
+
+export const CONTACT_ENGLISH_TEXTS = {
+  title: 'KEEP IN TOUCH',
+  paragraph: `Here are some ways that you can reach me, looking forward to talk with you all.`
+}
