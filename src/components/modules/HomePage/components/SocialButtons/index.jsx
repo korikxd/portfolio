38f5e67e7cf787865934ac9322fae2fc2a86ca31
korikxd <gabriel.garmer01@gmail.com>
2023-09-02 17:00:55 -0400

    💄 Added a hover animation effect

--- src/components/modules/HomePage/components/SocialButtons/index.jsx
+++ src/components/modules/HomePage/components/SocialButtons/index.jsx
@@ -2,9 +2,12 @@ import Link from 'next/link'
 
 const SocialButtons = ({ socials }) => {
   const renderButtons = () => (
-    <div className={`h-1/3 flex justify-center items-center`}>
+    <div className={`h-1/3 flex justify-center items-center gap-12`}>
       {socials.map((social, index) => (
-        <button className="mx-8" key={index}>
+        <button
+          key={index}
+          className={`flex justify-center items-center transform hover:-translate-y-3 border-2 w-12 h-12 rounded-full duration-500 hover:bg-${social.color} text-2xl`}
+        >
           <Link target="_blank" rel="noopener noreferrer" href={social.link}>
             {social.icon}
           </Link>
