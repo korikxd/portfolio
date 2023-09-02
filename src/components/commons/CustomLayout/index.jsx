ee7e247bf66dd274f342ee2023c75801845f92b5
korikxd <gabriel.garmer01@gmail.com>
2023-09-02 16:52:05 -0400

    ♻️ Removed unnecessary style prop

--- src/components/commons/CustomLayout/index.jsx
+++ src/components/commons/CustomLayout/index.jsx
@@ -4,7 +4,7 @@ import NavigationMenu from '../NavigationMenu'
 const CustomLayout = ({ children }) => {
   return (
     <div className={`flex flex-col h-screen`}>
-      <header className={`flex items-center justify-end py-8`}>
+      <header className={`flex justify-end py-8`}>
         <NavigationMenu />
       </header>
       <main className={`flex flex-col grow`}>
