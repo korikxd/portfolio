7858ab3f5118f38f8e6aebc394e32c5c91925861
korikxd <gabriel.garmer01@gmail.com>
2023-09-02 16:52:59 -0400

    🚧 Render hamburger menu on mobile resolutions

--- src/components/commons/NavigationMenu/index.jsx
+++ src/components/commons/NavigationMenu/index.jsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router'
 import SelectLanguage from './components/SelectLanguage'
 import ThemeButton from './components/ThemeButton'
 import { PAGE_ROUTES } from 'constants/routes'
+import { useState } from 'react'
 
 const NavigationMenu = () => {
   const {
@@ -14,28 +15,62 @@ const NavigationMenu = () => {
   const router = useRouter()
   const currentRoute = router.pathname
 
-  const homeItems = () => (
-    <div className={`flex items-center text-base leading-5`}>
-      <SelectLanguage currentLanguage={language} setCurrentLanguage={setLanguage} />
-      <ThemeButton />
-    </div>
-  )
-
   const defaultItems = () => (
-    <div className={`flex items-center text-base leading-5`}>
-      <div className={`hidden sm:block`}>
-        {PAGE_ROUTES.map((pageLink, index) => (
-          <Link href={pageLink.href} key={index} className={`p-1 font-medium sm:p-4`}>
+    <ul
+      className={`font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}
+    >
+      {PAGE_ROUTES.map((pageLink, index) => (
+        <li className={``} key={index}>
+          <Link
+            href={pageLink.href}
+            className={`block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500`}
+            aria-current="page"
+          >
             {language === 'ESPAÑOL' ? pageLink.spanishText : pageLink.englishText}
           </Link>
-        ))}
-      </div>
-      <SelectLanguage currentLanguage={language} setCurrentLanguage={setLanguage} />
-      <ThemeButton />
-    </div>
+        </li>
+      ))}
+    </ul>
   )
 
-  const itemsToRender = () => (currentRoute === '/' ? homeItems() : defaultItems())
+  const itemsToRender = () => (
+    <nav className={``}>
+      <div className={`max-w-screen-xl flex flex-wrap items-center gap-3 mx-auto p-4`}>
+        {currentRoute !== '/' && (
+          <>
+            <button
+              data-collapse-toggle="navbar-default"
+              className={`inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}
+              type="button"
+              aria-controls="navbar-default"
+              aria-expanded="false"
+            >
+              <svg
+                className={`w-5 h-5`}
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            <div id="navbar-default" className={`hidden w-full md:block md:w-auto`}>
+              {defaultItems()}
+            </div>
+          </>
+        )}
+        <SelectLanguage currentLanguage={language} setCurrentLanguage={setLanguage} />
+        <ThemeButton />
+      </div>
+    </nav>
+  )
 
   return itemsToRender()
 }
