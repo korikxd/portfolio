7045a59f7520c94d8f26e2d7328ff319d944fee6
korikxd <gabriel.garmer01@gmail.com>
2023-09-02 16:57:42 -0400

    ♻️ Moved resume object inside related folder

--- src/components/modules/ContactPage/constants.js
+++ src/components/modules/ContactPage/constants.js
@@ -1,3 +1,5 @@
+import { HiDocumentText } from 'react-icons/hi'
+
 export const CONTACT_SPANISH_TEXTS = {
   title: 'HABLEMOS',
   paragraph: `Siéntete libre de mandarme un mensaje, trataré de responder a la brevedad.`
@@ -6,4 +8,18 @@ export const CONTACT_SPANISH_TEXTS = {
 export const CONTACT_ENGLISH_TEXTS = {
   title: 'KEEP IN TOUCH',
   paragraph: `Here are some ways that you can reach me, looking forward to talk with you all.`
+}
+
+export const CV_SPANISH = {
+  key: 'cv',
+  label: 'Curriculum',
+  icon: <HiDocumentText />,
+  href: 'https://drive.google.com/file/d/1DM2m2w20j4sQqvygYVaFoXZSAXq4NalY/view'
+}
+
+export const CV_ENGLISH = {
+  key: 'resume',
+  label: 'Resume',
+  icon: <HiDocumentText />,
+  href: 'https://drive.google.com/file/d/1--lGKYKzXNEiTZ7cxCYaVrO-l0M0qGE-/view'
 }
