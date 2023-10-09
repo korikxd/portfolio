ede2974c744af1da60405180bee56fb1f38f44ca
korikxd <gabriel.garmer01@gmail.com>
2023-10-09 17:51:06 -0300

    🚧 Render differents banner depending on current theme

--- src/components/modules/HomePage/components/Banner/index.jsx
+++ src/components/modules/HomePage/components/Banner/index.jsx
@@ -1,8 +1,12 @@
+import { useTheme } from 'next-themes'
+
 const Banner = () => {
+  const { theme } = useTheme()
+
   return (
     <div
       className={`h-1/3 font-bold text-5xl flex flex-col items-center justify-center`}
-      style={{ backgroundImage: 'url(/banner.svg)' }}
+      style={{ backgroundImage: `url(/${theme === 'dark' ? 'dark_theme_banner.svg' : 'light_theme_banner.svg'}` }}
     >
       <p className={``}>Gabriel García Mercado</p>
       <div className={`w-max`}>
