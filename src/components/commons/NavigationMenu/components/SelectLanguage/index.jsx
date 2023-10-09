4f543f1b3399990ade895893365ad54ab2f0359a
korikxd <gabriel.garmer01@gmail.com>
2023-10-09 17:49:30 -0300

    🚧 Modified component bg colors

--- src/components/commons/NavigationMenu/components/SelectLanguage/index.jsx
+++ src/components/commons/NavigationMenu/components/SelectLanguage/index.jsx
@@ -7,7 +7,7 @@ const SelectLanguage = ({ currentLanguage, setCurrentLanguage }) => {
     <select
       defaultValue={currentLanguage}
       id="languages"
-      className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+      className={`bg-light-background border border-light-text text-light-text text-sm rounded-lg block p-2.5 dark:bg-dark-background dark:border-dark-text dark:text-dark-text`}
       onChange={(e) => handleLanguageSelection(e.target.value)}
     >
       {LANGUAGES.map((option) => (
