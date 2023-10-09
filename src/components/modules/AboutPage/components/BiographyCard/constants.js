c2d7ba14348e072c16b9fd0659c9680d45efec64
korikxd <gabriel.garmer01@gmail.com>
2023-10-09 17:49:52 -0300

    ✨ Render a new paragraph

--- src/components/modules/AboutPage/components/BiographyCard/constants.js
+++ src/components/modules/AboutPage/components/BiographyCard/constants.js
@@ -8,7 +8,8 @@ export const SPANISH_BIOGRAPHY_PARAGRAPHS = {
   },
   secondParagraph: `Me encanta desarrollar nuevos productos desde principio a fin, impartiendo buenas prácticas y aprendiendo cada día nuevas cosas.`,
   thirdParagraph: `En mis tiempos libres me gusta jugar videojuegos, escuchar música, compartir con amigos y mi familia.`,
-  memeQuote: `"No pases un cambio a producción un día viernes."`
+  memeQuote: `"No pases un cambio a producción un día viernes."`,
+  cite: `- Sun Tzu (Probablemente)`
 }
 
 export const ENGLISH_BIOGRAPHY_PARAGRAPHS = {
@@ -23,5 +24,6 @@ export const ENGLISH_BIOGRAPHY_PARAGRAPHS = {
    imparting best practices and learning new things every day.`,
   thirdParagraph: `In my free time, I like to play some videogames,
    listen to music and spend some time with friends and family.`,
-  memeQuote: `"Don't push to master branch on a Friday"`
+  memeQuote: `"Don't push to master branch on a Friday"`,
+  cite: `- Sun Tzu (Probably)`
 }
