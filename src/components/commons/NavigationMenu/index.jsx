5bd07cde364c082726c45425f18c579808d01654
korikxd <gabriel.garmer01@gmail.com>
2023-10-09 17:48:39 -0300

    ✨ Responsive mobile menu

--- src/components/commons/NavigationMenu/index.jsx
+++ src/components/commons/NavigationMenu/index.jsx
@@ -1,23 +1,45 @@
+import { useCallback, useEffect } from 'react'
 import Link from 'next/link'
-import usePortfolioContext from '@lib/Context/PortfolioContext'
+import { PAGE_ROUTES } from 'constants/routes'
 import { useRouter } from 'next/router'
 
+import usePortfolioContext from '@lib/Context/PortfolioContext'
+
+import ModalMobile from './components/ModalMobile'
 import SelectLanguage from './components/SelectLanguage'
 import ThemeButton from './components/ThemeButton'
-import { PAGE_ROUTES } from 'constants/routes'
-import { useState } from 'react'
 
 const NavigationMenu = () => {
   const {
-    portfolioContent: { language },
+    portfolioContent: { language, mobileMenu },
+    setMobileMenu,
     setLanguage
   } = usePortfolioContext()
   const router = useRouter()
   const currentRoute = router.pathname
 
+  const hideModal = useCallback(() => {
+    setMobileMenu(false)
+  }, [setMobileMenu])
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', hideModal)
+
+    return () => router.events.off('routeChangeStart', hideModal)
+  }, [hideModal, router.events])
+
+  const handleMobileMenu = () => setMobileMenu(!mobileMenu)
+
+  const renderHomeItems = () => (
+    <>
+      <SelectLanguage currentLanguage={language} setCurrentLanguage={setLanguage} />
+      <ThemeButton />
+    </>
+  )
+
   const defaultItems = () => (
     <ul
-      className={`font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700`}
+      className={`font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ${'hidden md:flex'}`}
     >
       {PAGE_ROUTES.map((pageLink, index) => (
         <li className={``} key={index}>
@@ -30,11 +52,13 @@ const NavigationMenu = () => {
           </Link>
         </li>
       ))}
+      <SelectLanguage currentLanguage={language} setCurrentLanguage={setLanguage} />
+      <ThemeButton />
     </ul>
   )
 
   const itemsToRender = () => (
-    <nav className={``}>
+    <nav>
       <div className={`max-w-screen-xl flex flex-wrap items-center gap-3 mx-auto p-4`}>
         {currentRoute !== '/' && (
           <>
@@ -43,7 +67,8 @@ const NavigationMenu = () => {
               className={`inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600`}
               type="button"
               aria-controls="navbar-default"
-              aria-expanded="false"
+              aria-expanded={mobileMenu ? 'true' : 'false'}
+              onClick={handleMobileMenu}
             >
               <svg
                 className={`w-5 h-5`}
@@ -61,13 +86,14 @@ const NavigationMenu = () => {
                 />
               </svg>
             </button>
-            <div id="navbar-default" className={`hidden w-full md:block md:w-auto`}>
-              {defaultItems()}
-            </div>
+            {mobileMenu && (
+              <ModalMobile language={language} setLanguage={setLanguage} handleMobileMenu={handleMobileMenu} />
+            )}
           </>
         )}
-        <SelectLanguage currentLanguage={language} setCurrentLanguage={setLanguage} />
-        <ThemeButton />
+        <div className={`md:flex items-center justify-center ${mobileMenu ? 'hidden' : 'block'} `}>
+          {currentRoute === '/' ? renderHomeItems() : defaultItems()}
+        </div>
       </div>
     </nav>
   )
