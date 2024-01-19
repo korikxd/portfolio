5fa071e89ebbc9047782426d4c741430eb946e70
korikxd <gabriel.garmer01@gmail.com>
2024-01-19 14:37:00 -0300

    🔥 Remove comments

--- src/components/modules/AboutPage/components/ExperienceCard/index.jsx
+++ src/components/modules/AboutPage/components/ExperienceCard/index.jsx
@@ -4,14 +4,12 @@ const ExperienceCard = ({ experiences }) => {
   const renderExperiences = () => (
     <div className={`relative col-span-12 px-4 space-y-6 sm:col-span-9`}>
       <div
-        //TODO --> Fix bg color
-        className={`col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-`}
+        className={`col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-firstAccent`}
       >
         {experiences.jobs.map((work, index) => (
           <div
             key={index}
-            //TODO --> Fix bg color
-            className={`flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-`}
+            className={`flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-firstAccent`}
           >
             <h3 className={`text-xl font-semibold`}>
               <a href={work.link} target="_blank" rel="noopener noreferrer">
