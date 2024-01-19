ef747cda23b2fa6b8830b25d1aeefb17536b44ed
korikxd <gabriel.garmer01@gmail.com>
2024-01-19 14:40:07 -0300

    ♻️ Removed comments & modified colors

--- src/components/modules/ContactPage/components/ContactButton/index.jsx
+++ src/components/modules/ContactPage/components/ContactButton/index.jsx
@@ -1,8 +1,6 @@
 const ContactButton = ({ href, text, icon }) => (
   <a className={``} href={href} target="_blank" rel="noopener noreferrer">
-    {/* TODO --> Fix text color */}
-    <button type="button" className={`flex items-center font-bold uppercase p-3 rounded bg-`}>
-      {/* TODO Verify colors */}
+    <button type="button" className={`flex items-center font-bold uppercase p-3 rounded bg-firstAccent`}>
       <span className={``}>{icon}</span>
       <span className={``}>{text}</span>
     </button>
