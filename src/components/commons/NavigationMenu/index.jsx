3477ff9edc21da7c6c85303f8d3b247916f42dd6
korikxd <gabriel.garmer01@gmail.com>
2024-01-19 14:35:06 -0300

    ♻️ Modified navbar menu behavior

--- src/components/commons/NavigationMenu/index.jsx
+++ src/components/commons/NavigationMenu/index.jsx
@@ -39,13 +39,13 @@ const NavigationMenu = () => {
 
   const defaultItems = () => (
     <ul
-      className={`font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ${'hidden md:flex'}`}
+      className={`font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 ${'hidden md:flex'}`}
     >
       {PAGE_ROUTES.map((pageLink, index) => (
         <li className={``} key={index}>
           <Link
             href={pageLink.href}
-            className={`block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500`}
+            className={`block py-2 pl-3 pr-4 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500`}
             aria-current="page"
           >
             {language === 'ESPAÑOL' ? pageLink.spanishText : pageLink.englishText}
