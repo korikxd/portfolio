af4da70abc149147f6c31f9cafd2c796bdf833c8
korikxd <gabriel.garmer01@gmail.com>
2024-01-19 14:40:40 -0300

    ♻️ Modified banner styles

--- src/components/modules/HomePage/components/Banner/index.jsx
+++ src/components/modules/HomePage/components/Banner/index.jsx
@@ -1,19 +1,10 @@
-import { useTheme } from 'next-themes'
-
-const Banner = () => {
-  const { theme } = useTheme()
-
-  return (
-    <div
-      className={`h-1/3 font-bold text-5xl flex flex-col items-center justify-center`}
-      style={{ backgroundImage: `url(/${theme === 'dark' ? 'dark_theme_banner.svg' : 'light_theme_banner.svg'}` }}
-    >
-      <p className={``}>Gabriel García Mercado</p>
-      <div className={`w-max`}>
-        <p className={`animate-typing overflow-hidden whitespace-nowrap border-r-4 pr-5`}>Frontend Developer</p>
-      </div>
+const Banner = () => (
+  <div className={`h-1/3 font-bold text-5xl flex flex-col items-center justify-center`}>
+    <p className={``}>Gabriel García Mercado</p>
+    <div className={`w-max`}>
+      <p className={`animate-typing overflow-hidden whitespace-nowrap border-r-4 pr-5`}>Front End Developer</p>
     </div>
-  )
-}
+  </div>
+)
 
 export default Banner
