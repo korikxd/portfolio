86d4008fec5030e34c4bad8fb8db10608de44365
korikxd <gabriel.garmer01@gmail.com>
2025-06-27 00:02:44 -0400

    🎨 Easy component structure

--- src/components/modules/HomePage/index.jsx
+++ src/components/modules/HomePage/index.jsx
@@ -1,23 +1,9 @@
-import { useEffect } from 'react';
 import Banner from './components/Banner';
-import NavigationButtons from './components/NavigationButtons';
-import { ENGLISH_TEXTS, SPANISH_TEXTS } from './components/NavigationButtons/constants';
 
-const HomePage = ({ homeData, setHomeData, language }) => {
-  useEffect(() => {
-    if (language !== 'ESPAÑOL') {
-      setHomeData({ ...homeData, navigationButtons: ENGLISH_TEXTS });
-    } else {
-      setHomeData({ ...homeData, navigationButtons: SPANISH_TEXTS });
-    }
-  }, [language]);
-
-  return (
-    <div className={`h-full`}>
-      <Banner />
-      <NavigationButtons buttons={homeData.navigationButtons} />
-    </div>
-  );
-};
+const HomePage = () => (
+  <div className={`h-full`}>
+    <Banner />
+  </div>
+)
 
 export default HomePage;
